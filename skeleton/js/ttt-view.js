57398c9f366fd808d3e483d78e5d5cc233c6fce6
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-30 13:23:43 -0700

    Middle of the day

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -3,15 +3,27 @@
     window.TTT = {};
   }
 
-  var View = TTT.View = function (game, $el) {
+  var View = window.TTT.View = function (game, $el) {
+    this.$el = $el;
+    this.game = game;
+    this.setupBoard();
   };
 
   View.prototype.bindEvents = function () {
+    
   };
 
   View.prototype.makeMove = function ($square) {
   };
 
   View.prototype.setupBoard = function () {
+    var list = this.$el;
+    for (var i = 0; i < 9; i++) {
+      var item = list.append($('<li>'));
+      var square = item.append($('<div>'));
+      square.addClass('square');
+    }
+    list.addClass("board");
+    return list;
   };
 })();
